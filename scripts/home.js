2e0f322e6d08dbcbe32fcc57b7d9fd0f11b5910c
Nicko Enargan <donenargan@gmail.com>
2025-02-28 20:59:27 +0800

    Bugs fixes, script file didnt work making the web site buggy or unsuable. Tried fixing it

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,4 @@
-import animejs from "./anime.min.js";
-
 console.log("home.js is working");
-const anime = animejs;
 
 const aboutme = document.querySelector("#about-me");
 const skills = document.querySelector("#skills");
@@ -26,33 +23,4 @@ projects.addEventListener("click", (e) => {
   setTimeout(() => {
     window.location.href = "works.html";
   }, 200);
-});
-
-anime({
-  targets: "#projects",
-  translateX: -500,
-  duration: 500,
-  opacity: 1,
-  delay: anime.stagger(100), // increase delay by 100ms for each elements.
-});
-anime({
-  targets: ".desc-container",
-  translateX: 500,
-  opacity: 1,
-  duration: 1200,
-  delay: anime.stagger(100, { start: 300, easing: "easeOutQuad" }), // increase delay by 100ms for each elements.
-});
-
-anime({
-  targets: ".words, .project-label",
-  opacity: 1,
-  duration: 1500,
-  delay: anime.stagger(150),
-});
-
-anime({
-  targets: ".project-label , .words",
-  textShadow: "8px 0 rgba(255, 255, 255, 0.5)",
-  duration: 1500,
-  delay: anime.stagger(75, { start: 950 }),
 });
--- scripts/work.js
+++ scripts/work.js
@@ -1,14 +1,6 @@
-import anime from "./anime.min.js";
 window.redirectUser = redirectUser;
 
 function redirectUser(url) {
   window.location.href = url;
 }
-anime({
-  targets: ".card",
-  opacity: 1,
-  duration: 450,
-  easing: "linear",
-  background: "#1b1a55",
-  delay: anime.stagger(100, { start: 100 }),
-});
+console.log("work.js is working");
